2f8b8056428c945bc2071fba20ac9a3730bd8c99
Rishab Karki <nullrish@gmail.com>
2026-07-30 11:44:00 +0545

    Updated place flower without extra pointers

--- 605_can-place-flowers.py
+++ 605_can-place-flowers.py
@@ -1,22 +1,22 @@
 class Solution:
     def canPlaceFlowers(self, flowerbed: list[int], n: int) -> bool:
+        if n == 0:
+            return True
         size = len(flowerbed)
-        i = 0
-        next = 0
-        while i < size:
+        for i in range(size):
+            if i == 0 and flowerbed[i] == 1:
+                continue
+            if i + 1 <= size - 1 and flowerbed[i + 1] == 1:
+                continue
+            if i - 1 >= 0 and flowerbed[i - 1] == 1:
+                continue
+            if flowerbed[i] == 1:
+                continue
+            n -= 1
+            flowerbed[i] = 1
+            print(flowerbed)
             if n == 0:
                 return True
-            if i + 1 <= size - 1:
-                next = i + 1
-            if flowerbed[i] == 1:
-                i += 2
-                continue
-            if flowerbed[i] == 0 and flowerbed[next] == 0:
-                flowerbed[i] = 1
-                n -= 1
-                i = i + 2
-                continue
-            i += 1
         return False
 
 
